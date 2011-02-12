056d9196c8df3582f9db5b88456412f069e8bb8d
Arne99 <ArneLandwehr@googlemail.com>
2011-02-12 17:39:06 +0100

    finished the server administration view

--- Code/scjd.urlybird/src/main/java/suncertify/admin/gui/ServerAdminGui.java
+++ Code/scjd.urlybird/src/main/java/suncertify/admin/gui/ServerAdminGui.java
@@ -2,53 +2,172 @@ package suncertify.admin.gui;
 
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Observable;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
-public class ServerAdminGui extends JFrame {
+public class ServerAdminGui extends Observable {
 
     private static final long serialVersionUID = 4762410272117483890L;
 
-    public ServerAdminGui(final String name) {
-	super(name);
-    }
+    final JFrame mainFrame = new JFrame();
+    final JPanel databasePanel = new JPanel();
+    final JLabel databasePathLabel = new JLabel();
+    final JTextField dbPathTextField = new JTextField();
+    final JButton dbPathButton = new JButton();
+    final JPanel connectionPanel = new JPanel();
+    final JLabel hostLabel = new JLabel();
+    final JTextField hostConnectionTextField = new JTextField();
+    final JLabel portLabel = new JLabel();
+    final JTextField portTextField = new JTextField();
+    final JButton startServerButton = new JButton();
+    final JLabel serverIsLabel = new JLabel();
+    final JLabel serverStatusLabel = new JLabel();
 
-    public static void start() {
-	final ServerAdminGui serverAdminGui = new ServerAdminGui(
-		"Server Administration");
-	serverAdminGui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    public void startGui() {
 
-	final Container contentPane = serverAdminGui.getContentPane();
+	mainFrame.setTitle("Server Administration");
+	mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+	final Container contentPane = mainFrame.getContentPane();
 	contentPane.setLayout(new GridBagLayout());
-	final GridBagConstraints c = new GridBagConstraints();
+	GridBagConstraints gridBagConstraints;
 
-	final JLabel dbLocationText = new JLabel("DB Location: ");
-	c.gridx = 0;
-	c.gridy = 0;
-	contentPane.add(dbLocationText, c);
+	databasePanel.setBorder(BorderFactory.createTitledBorder("Database"));
+	databasePanel.setLayout(new GridBagLayout());
 
-	final JTextField dbPathField = new JTextField(20);
-	c.gridx = 1;
-	c.gridy = 0;
-	c.gridwidth = GridBagConstraints.RELATIVE;
-	contentPane.add(dbPathField, c);
+	databasePathLabel.setText("Path");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 0;
+	gridBagConstraints.gridy = 0;
+	gridBagConstraints.insets = new Insets(52, 26, 0, 0);
+	databasePanel.add(databasePathLabel, gridBagConstraints);
 
-	final JButton dbPathButton = new JButton("Select");
-	c.gridx = 20;
-	c.gridy = 0;
-	contentPane.add(dbPathButton, c);
+	dbPathTextField.setText("");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 1;
+	gridBagConstraints.gridy = 0;
+	gridBagConstraints.gridheight = 2;
+	gridBagConstraints.ipadx = 250;
+	gridBagConstraints.insets = new Insets(46, 42, 0, 26);
+	databasePanel.add(dbPathTextField, gridBagConstraints);
 
-	dbPathField.setEnabled(false);
+	dbPathButton.setText("Select");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 1;
+	gridBagConstraints.gridy = 2;
+	gridBagConstraints.insets = new Insets(7, 225, 6, 26);
+	databasePanel.add(dbPathButton, gridBagConstraints);
+
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 0;
+	gridBagConstraints.gridy = 2;
+	gridBagConstraints.gridwidth = 5;
+	gridBagConstraints.ipadx = 12;
+	gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+	gridBagConstraints.insets = new Insets(24, 20, 0, 20);
+	contentPane.add(databasePanel, gridBagConstraints);
+
+	connectionPanel.setBorder(BorderFactory
+		.createTitledBorder("Connection"));
+	connectionPanel.setMinimumSize(new Dimension(360, 150));
+	connectionPanel.setLayout(new GridBagLayout());
+
+	hostLabel.setText("Host");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 0;
+	gridBagConstraints.gridy = 0;
+	gridBagConstraints.insets = new Insets(79, 32, 0, 0);
+	connectionPanel.add(hostLabel, gridBagConstraints);
+
+	hostConnectionTextField.setText("");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 2;
+	gridBagConstraints.gridy = 0;
+	gridBagConstraints.gridheight = 2;
+	gridBagConstraints.ipadx = 242;
+	gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+	gridBagConstraints.insets = new Insets(73, 10, 0, 53);
+	connectionPanel.add(hostConnectionTextField, gridBagConstraints);
+
+	portLabel.setText("Port");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 0;
+	gridBagConstraints.gridy = 2;
+	gridBagConstraints.gridwidth = 2;
+	gridBagConstraints.ipadx = 33;
+	gridBagConstraints.insets = new Insets(14, 32, 0, 0);
+	connectionPanel.add(portLabel, gridBagConstraints);
+
+	portTextField.setText("");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 2;
+	gridBagConstraints.gridy = 2;
+	gridBagConstraints.gridheight = 2;
+	gridBagConstraints.ipadx = 242;
+	gridBagConstraints.insets = new Insets(8, 10, 19, 53);
+	connectionPanel.add(portTextField, gridBagConstraints);
+
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 0;
+	gridBagConstraints.gridy = 3;
+	gridBagConstraints.gridwidth = 5;
+	gridBagConstraints.ipadx = 47;
+	gridBagConstraints.ipady = 13;
+	gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+	gridBagConstraints.insets = new Insets(14, 20, 0, 20);
+	contentPane.add(connectionPanel, gridBagConstraints);
+
+	startServerButton.setText("Start Server");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 0;
+	gridBagConstraints.gridy = 4;
+	gridBagConstraints.gridwidth = 2;
+	gridBagConstraints.ipadx = 22;
+	gridBagConstraints.ipady = 26;
+	gridBagConstraints.insets = new Insets(29, 156, 17, 0);
+	contentPane.add(startServerButton, gridBagConstraints);
+
+	serverIsLabel.setFont(new Font(Font.SANS_SERIF, 0, 24));
+	serverIsLabel.setText("Server is:");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 0;
+	gridBagConstraints.gridy = 0;
+	gridBagConstraints.gridheight = 2;
+	gridBagConstraints.ipady = 18;
+	gridBagConstraints.insets = new Insets(52, 56, 0, 0);
+	contentPane.add(serverIsLabel, gridBagConstraints);
+
+	serverStatusLabel.setFont(new Font(Font.SANS_SERIF, 0, 24));
+	serverStatusLabel.setText("Running");
+	gridBagConstraints = new GridBagConstraints();
+	gridBagConstraints.gridx = 1;
+	gridBagConstraints.gridy = 0;
+	gridBagConstraints.gridwidth = 3;
+	gridBagConstraints.ipadx = 29;
+	gridBagConstraints.ipady = 10;
+	gridBagConstraints.insets = new Insets(56, 33, 0, 0);
+	contentPane.add(serverStatusLabel, gridBagConstraints);
+
+	mainFrame.setPreferredSize(new Dimension(480, 580));
+	mainFrame.setMinimumSize(new Dimension(480, 580));
+	mainFrame.setLocationRelativeTo(null);
+	mainFrame.setResizable(false);
+	mainFrame.setVisible(true);
 
 	final JFileChooser dbPathChooser = new JFileChooser();
 	dbPathButton.addActionListener(new ActionListener() {
@@ -58,17 +177,13 @@ public class ServerAdminGui extends JFrame {
 		final int returnVal = dbPathChooser.showDialog(contentPane,
 			"Select");
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-		    dbPathField.setText(dbPathChooser.getSelectedFile()
+		    dbPathTextField.setText(dbPathChooser.getSelectedFile()
 			    .getAbsolutePath());
 		}
 
 	    }
 	});
 
-	serverAdminGui.setLocationRelativeTo(null);
-	serverAdminGui.setPreferredSize(new Dimension(500, 100));
-	serverAdminGui.pack();
-	serverAdminGui.setVisible(true);
     }
 
     public static void main(final String[] args) {
@@ -76,7 +191,7 @@ public class ServerAdminGui extends JFrame {
 
 	    @Override
 	    public void run() {
-		ServerAdminGui.start();
+		new ServerAdminGui().startGui();
 	    }
 	});
     }
