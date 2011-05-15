e33552ba1e8153e570dcce8e7c6d3d25a1badf06
Arne99 <ArneLandwehr@googlemail.com>
2011-05-15 12:32:44 +0200

    added some more JavaDoc

--- Code/scjd.urlybird/src/main/java/suncertify/domain/ConstraintViolationException.java
+++ Code/scjd.urlybird/src/main/java/suncertify/domain/ConstraintViolationException.java
@@ -1,7 +1,27 @@
 package suncertify.domain;
 
-public class ConstraintViolationException extends Exception {
+/**
+ * Is thrown if a constraint of an domain object attribute is not met. For
+ * example if the attribute age must be positive and the client want to store a
+ * negative value.
+ * 
+ * @author arnelandwehr
+ * 
+ */
+public final class ConstraintViolationException extends Exception {
 
+    /**
+     * the SUID.
+     */
+    private static final long serialVersionUID = 6677944220649967272L;
+
+    /**
+     * Constructs a new <code>ConstraintViolationException</code> with the given
+     * message.
+     * 
+     * @param message
+     *            the message to be displayed, must not be <code>null</code>.
+     */
     public ConstraintViolationException(final String message) {
 	super(message);
     }
--- Code/scjd.urlybird/src/main/java/suncertify/domain/DataAccessObject.java
+++ Code/scjd.urlybird/src/main/java/suncertify/domain/DataAccessObject.java
@@ -1,17 +1,48 @@
 package suncertify.domain;
 
 import java.util.List;
-import java.util.Map;
-
-import suncertify.common.RoomOffer;
 import suncertify.db.DuplicateKeyException;
+import suncertify.db.Record;
 import suncertify.db.RecordNotFoundException;
 
+/**
+ * A <code>DataAccessObject</code> (DAO) is the bridge between the domain layer
+ * and the persistence layer. A DAO could load domain objects of a specified
+ * type from the database an store it finally there.
+ * 
+ * @author arnelandwehr
+ * 
+ * @param <T>
+ *            the type of the domain object this dao can handle.
+ */
 public interface DataAccessObject<T> {
 
+    /**
+     * Creates a new domain object and stores it in the database.
+     * 
+     * @param values
+     *            the values of the domain object, must not be <code>null</code>
+     *            .
+     * @return the created and stored domain object.
+     * @throws DuplicateKeyException
+     *             if the domain object is already stored in the database.
+     * @throws ConstraintViolationException
+     *             if the given values violates any attribute condition of the
+     *             domain object to create.
+     */
     T create(List<String> values) throws DuplicateKeyException,
 	    ConstraintViolationException;
 
+    /**
+     * Deletes the {@link Record} with the given index from the database.
+     * 
+     * @param index
+     *            the index of the <code>Record</code> to delete.
+     * @param lock
+     *            the lockId
+     * @throws RecordNotFoundException
+     * @throws suncertify.db.SecurityException
+     */
     void delete(int index, long lock) throws RecordNotFoundException,
 	    suncertify.db.SecurityException;
 
