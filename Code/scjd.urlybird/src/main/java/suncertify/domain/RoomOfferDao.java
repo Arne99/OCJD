7344ce24220064f09e10e71d07951d8bc7169bef
Arne99 <ArneLandwehr@googlemail.com>
2011-05-15 20:08:56 +0200

    added some JavaDoc

--- Code/scjd.urlybird/src/main/java/suncertify/domain/RoomOfferDao.java
+++ Code/scjd.urlybird/src/main/java/suncertify/domain/RoomOfferDao.java
@@ -14,25 +14,46 @@ import suncertify.db.DuplicateKeyException;
 import suncertify.db.RecordNotFoundException;
 import suncertify.db.SecurityException;
 
+/**
+ * The <code>RoomOfferDao</code> is the bridge between the stored
+ * <code>RoomOffer</code> objects in the database and the <code>RoomOffer</code>
+ * domain objects. It offers the needed CRUD operations on
+ * <code>RoomOffers</code> and abstracts the persistence layer from the domain
+ * layer.
+ * 
+ * 
+ * @author arnelandwehr
+ * 
+ */
 final class RoomOfferDao implements DataAccessObject<UrlyBirdRoomOffer> {
 
+    /** the database where the <code>RoomOffers</code> are stored. */
     private final DB database;
 
-    private final RoomOfferFactory builder;
+    /** the factory for <code>RoomOffer</code>. */
+    private final RoomOfferFactory factory;
 
-    RoomOfferDao(final DB database, final RoomOfferFactory builder) {
+    /**
+     * Construct a new <code>RoomOfferDao</code>.
+     * 
+     * @param database
+     *            the database, must not be <code>null</code>.
+     * @param roomOfferFactory
+     *            the factory, must not be <code>null</code>.
+     */
+    RoomOfferDao(final DB database, final RoomOfferFactory roomOfferFactory) {
 	this.database = database;
-	this.builder = builder;
+	this.factory = roomOfferFactory;
     }
 
     @Override
     public UrlyBirdRoomOffer create(final List<String> values)
 	    throws DuplicateKeyException, ConstraintViolationException {
 
-	builder.checkValues(values);
+	factory.checkValues(values);
 	final int index = database.create(values.toArray(new String[values
 		.size()]));
-	return builder.createRoomOffer(values, index);
+	return factory.createRoomOffer(values, index);
     }
 
     @Override
@@ -72,7 +93,7 @@ final class RoomOfferDao implements DataAccessObject<UrlyBirdRoomOffer> {
     public UrlyBirdRoomOffer read(final int index)
 	    throws RecordNotFoundException, ConstraintViolationException {
 	final String[] values = database.read(index);
-	final UrlyBirdRoomOffer roomOffer = builder.createRoomOffer(
+	final UrlyBirdRoomOffer roomOffer = factory.createRoomOffer(
 		Arrays.asList(values), index);
 
 	return roomOffer;
@@ -88,11 +109,21 @@ final class RoomOfferDao implements DataAccessObject<UrlyBirdRoomOffer> {
     public void update(final UrlyBirdRoomOffer roomOffer, final long lock)
 	    throws RecordNotFoundException, SecurityException {
 
-	database.update(roomOffer.getIndex(), toPersistableArray(roomOffer),
-		lock);
+	database.update(roomOffer.getIndex(),
+		transformToPersistableForm(roomOffer), lock);
     }
 
-    private String[] toPersistableArray(final UrlyBirdRoomOffer roomOffer) {
+    /**
+     * Transforms the given <code>RoomOffer</code> domain object to a pesistable
+     * String array.
+     * 
+     * @param roomOffer
+     *            the <code>RoomOffer</code> to transform.
+     * @return the persisable form of the given domain object as an
+     *         <code>String</code> array.
+     */
+    private String[] transformToPersistableForm(
+	    final UrlyBirdRoomOffer roomOffer) {
 	final String roomSize = convertIntToPersistableString(roomOffer
 		.getRoomSize());
 	final String smokingAllowed = convertBooleanToPersistableString(roomOffer
@@ -107,24 +138,51 @@ final class RoomOfferDao implements DataAccessObject<UrlyBirdRoomOffer> {
 		roomOffer.getCustomerId() };
     }
 
-    private String convertBooleanToPersistableString(
-	    final boolean smokingAllowed) {
-	return (smokingAllowed) ? "Y" : "N";
+    /**
+     * Converts the given boolean to a persistable <code>String</code> form.
+     * 
+     * @param aBool
+     *            the boolean value
+     * @return "Y" if aBool is true, else "N".
+     */
+    private String convertBooleanToPersistableString(final boolean aBool) {
+	return (aBool) ? "Y" : "N";
     }
 
-    private String convertDateToPersistableString(final Date bookableDate) {
+    /**
+     * Converts the given {@link Date} to an persistable <code>String</code>
+     * form.
+     * 
+     * @param aDate
+     *            the date to transform.
+     * @return the String representation of the form "yyyy/MM/dd".
+     */
+    private String convertDateToPersistableString(final Date aDate) {
 
-	return new SimpleDateFormat("yyyy/MM/dd", Locale.US)
-		.format(bookableDate);
+	return new SimpleDateFormat("yyyy/MM/dd", Locale.US).format(aDate);
     }
 
-    private String convertIntToPersistableString(final int roomSize) {
-	return "" + roomSize;
+    /**
+     * Converts the given <code>int</code> to an <code>String</code>.
+     * 
+     * @param anInt
+     *            the <code>int</code>.
+     * @return the <code>int</code> as a <code>String</code>.
+     */
+    private String convertIntToPersistableString(final int anInt) {
+	return "" + anInt;
     }
 
-    private String convertMoneyToPersistableString(final Money price) {
-	final String symbol = price.getCurreny().getSymbol(Locale.US);
-	final BigDecimal amount = price.getAmount();
+    /**
+     * Converts the given {@link Money} to an persistable String form.
+     * 
+     * @param someMoney
+     *            the <code>Money</code> to transform.
+     * @return the persiatable String representation.
+     */
+    private String convertMoneyToPersistableString(final Money someMoney) {
+	final String symbol = someMoney.getCurreny().getSymbol(Locale.US);
+	final BigDecimal amount = someMoney.getAmount();
 	final String value = amount.setScale(2).toPlainString();
 	return symbol + value;
     }
